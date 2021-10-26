8ba1fd3ca2e3bdb4df86e77605efaaa6ceb95a56
Julia Togashi <jutogashi@hotmail.com>
2021-10-26 21:31:28 +0200

    update select function

--- Table.py
+++ Table.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 class Table:
-    def __init__(self, schema, n_rows, name, storage="row"):
+    def __init__(self, schema, n_rows, name, storage="row", filename=None):
         self.name = name
         self.n_cols = len(schema)
         self.storage = storage
@@ -11,14 +11,18 @@ class Table:
         self.col_names = [p[0] for p in schema.items()]
         self.dtypes = [p[1] for p in schema.items()]
         self.n_rows = n_rows
+        self.filename = filename
         if self.storage == "row":
             self.data = np.empty(self.n_rows, dtype=object)
-        else:
+        elif self.storage == "col":
             self.data = [np.empty(self.n_rows, dtype=column[1]) for column in self.schema.items()]
+        else:
+            self.data = None
 
     def load_csv(self, filename):
         df = pd.read_csv(filename, delimiter='|', header=None).iloc[:, :-1]
         self.n_rows = len(df)
+        self.filename = filename
         if self.storage == "row":
             self.data = df.values
         else:
